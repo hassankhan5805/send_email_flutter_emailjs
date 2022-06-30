bd986df049c918aa95abb6a59c3982c3738974bd
muhammad hassan <hassankhan5805@gmail.com>
2022-06-30 16:53:09 -0700

    Update emailjs.dart

--- emailjs.dart
+++ emailjs.dart
@@ -2,9 +2,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future send(String name,String email) async {
-  final servicesID = "hassankhan5805";
-  final templateID = "template_dc3zwuo";
-  final userID = "UXr24732yyTTHmHY9";
+  final servicesID = "";
+  final templateID = "";
+  final userID = "";//you will find this in account as a public key
   final url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
   final response = await http
       .post(url,
@@ -13,7 +13,7 @@ Future send(String name,String email) async {
             'service_id': servicesID,
             'template_id': templateID,
             'user_id': userID,
-            // 'accessToken': "4gez68Vuwd1f_tlax-j5c",
+            // 'accessToken': "",
             'template_params': {
               'user_name': name,
               'user_email': email,
